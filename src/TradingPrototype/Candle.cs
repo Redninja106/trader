018c172b2a8c559ec6c67754b9152bbd9225232d
Ryan Andersen <redninja106@live.com>
2022-10-23 19:51:21 -0400

    Add Candle.CreateTicks()

--- src/TradingPrototype/Candle.cs
+++ src/TradingPrototype/Candle.cs
@@ -117,4 +117,44 @@ internal class Candle : ICandle
 
         return c;
     }
+
+    public static Tick[] CreateTicks(ICandle candle, int tickCount)
+    {
+        if (tickCount <= 4)
+        {
+            throw new ArgumentException("tickCount must be greater than 4", nameof(tickCount));
+        }
+
+        // calculate prices
+        decimal[] ticks = new decimal[tickCount];
+        for (int i = 0; i < tickCount; i++)
+        {
+            decimal price = 0;
+            decimal progress = i / (tickCount - 1m);
+            progress *= 3;
+
+            decimal x = progress % 1.0m;
+            var (m, b) = progress switch
+            {
+                <= 0 => (0m, candle.Open),
+                <= 1 => (candle.High - candle.Open, candle.Open),
+                <= 2 => (candle.Low - candle.High, candle.High),
+                <= 3 => (candle.Close - candle.Low, candle.Low),
+                _ => (0m, candle.Close)
+            };
+
+            decimal y = m * x + b;
+
+            ticks[i] = price;
+        }
+
+        // calculate volume
+        long[] volume = new long[tickCount];
+        for (int i = 0; i < tickCount; i++)
+        {
+            volume[i] = candle.Volume / tickCount;
+        }
+
+        return ticks.Zip(volume).Select(t => new Tick(t.First, t.Second)).ToArray();
+    }
 }
--- src/TradingPrototype/Tick.cs
+++ src/TradingPrototype/Tick.cs
@@ -2,6 +2,12 @@
 
 public class Tick
 {
+    public Tick(decimal price, long volume)
+    {
+        Price = price;
+        Volume = volume;
+    }
+
     public decimal Price { get; set; }
     public long Volume { get; set; }
 }
